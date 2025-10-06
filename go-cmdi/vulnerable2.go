1d58c3a32b1ef48a96bc9240a5a6946bfb72c70d
Bahar Shah <bahar.shah@datadoghq.com>
2025-10-06 15:49:27 -0400

    Update vulnerable2.go

--- go-cmdi/vulnerable2.go
+++ go-cmdi/vulnerable2.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	userInput := os.Args[1]
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	
 	defer cancel()
 	
 	cmd := exec.CommandContext(ctx, "ping", "-c", "1", userInput)
