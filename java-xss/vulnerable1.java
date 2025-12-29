11a3564678fcd50d8cfecdfbce3de3fdefe639d9
Jason Foral <jason.foral@datadoghq.com>
2025-12-29 14:01:13 -0500

    Cache bust

--- java-xss/vulnerable1.java
+++ java-xss/vulnerable1.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/search")
 public class vulnerable1 extends HttpServlet {
     
+    // Cache bust: 1
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
