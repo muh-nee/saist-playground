7990f9eb408d993db7e3d7174ffa7bacd7515ea4
Jon Green <jon.green@datadoghq.com>
2026-06-17 13:05:56 -0400

    triggering a scan

--- go-xss/secure2.go
+++ go-xss/secure2.go
@@ -12,7 +12,7 @@ import (
 func validateInput(input string) string {
 	re := regexp.MustCompile(`(?i)<script[^>]*>.*?</script>`)
 	cleaned := re.ReplaceAllString(input, "")
-	
+
 	return html.EscapeString(cleaned)
 }
 
@@ -21,23 +21,23 @@ func main() {
 
 	app.Get("/comment", func(c *fiber.Ctx) error {
 		comment := c.Query("text")
-		
+
 		safeComment := validateInput(comment)
-		
+
 		tmpl := template.Must(template.New("comment").Parse(`<html><body>
 		<h2>Your Comment</h2>
 		<div class="comment">{{.Comment}}</div>
 		<p><a href="/">Back to home</a></p>
 		</body></html>`))
-		
+
 		data := struct {
 			Comment string
 		}{
 			Comment: safeComment,
 		}
-		
+
 		c.Set("Content-Type", "text/html")
-		
+
 		var buf strings.Builder
 		tmpl.Execute(&buf, data)
 		return c.SendString(buf.String())
@@ -45,9 +45,9 @@ func main() {
 
 	app.Get("/welcome", func(c *fiber.Ctx) error {
 		name := c.Query("name")
-		
+
 		safeName := validateInput(name)
-		
+
 		tmpl := template.Must(template.New("welcome").Parse(`<html><body>
 		<h1>Welcome Page</h1>
 		<div id="welcome-message"></div>
@@ -56,15 +56,15 @@ func main() {
 		document.getElementById('welcome-message').textContent = "Hello " + userName + "!";
 		</script>
 		</body></html>`))
-		
+
 		data := struct {
 			Name string
 		}{
 			Name: safeName,
 		}
-		
+
 		c.Set("Content-Type", "text/html")
-		
+
 		var buf strings.Builder
 		tmpl.Execute(&buf, data)
 		return c.SendString(buf.String())
