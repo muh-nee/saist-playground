4da06fd7b90df6274624216b647f6b0507ebe187
Jon Green <jon.green@datadoghq.com>
2026-07-01 17:15:16 -0400

    cleanup test files

--- python-sensitiveinfodisclosure/auth.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-def authenticate(username, password):
-    logger.info(f"Authenticating user {username} with password {password}")
-    # ... auth logic
-    return True
