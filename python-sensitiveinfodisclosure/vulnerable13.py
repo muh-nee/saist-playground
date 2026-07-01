addf66e2027a8c8543f91fba5b267bb897f7c3c6
Jon Green <jon.green@datadoghq.com>
2026-07-01 16:36:21 -0400

    add python SID test file

---
+++ python-sensitiveinfodisclosure/vulnerable13.py
@@ -0,0 +1,7 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+def authenticate(username, password):
+    logger.info(f"Authenticating user {username} with password {password}")
+    return True
