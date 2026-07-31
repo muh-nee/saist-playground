12ca11447c4edc02cea3fb7c29ac0ce82e58b009
Joshua Delgado <joshua.delgado@datadoghq.com>
2026-07-31 09:37:19 -0400

    more

---
+++ go-cmdi-3.go
@@ -0,0 +1,11 @@
+// this is a change
+package main
+
+import (
+	"os/exec"
+)
+
+func run(userInput string) {
+	// Command injection: user input passed straight to a shell.
+	exec.Command("sh", "-c", "echo "+userInput).Run()
+}
