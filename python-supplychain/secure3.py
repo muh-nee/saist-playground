e6ec2f97711ee3c0b4144604a8424daaece6b25e
Jon Green <jon.green@datadoghq.com>
2026-07-31 17:12:17 -0400

    Update secure3.py to use full commit SHA for revision= sanitizer

--- python-supplychain/secure3.py
+++ python-supplychain/secure3.py
@@ -3,6 +3,6 @@ from transformers import AutoModel
 def load_model():
     model = AutoModel.from_pretrained(
         "openai-community/gpt2",
-        revision="v1.0",
+        revision="607a30d783dfa663caf39e06633721c8d4cfcd7e",
     )
     return model
