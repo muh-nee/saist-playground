1462c3bead0f92142044b331fae479df7c90dadf
Jon Green <jon.green@datadoghq.com>
2026-08-03 15:09:49 -0400

    Fix vulnerable11.java: add proper zip extraction so taint flows to SavedModelBundle.load

--- java-supplychain/vulnerable11.java
+++ java-supplychain/vulnerable11.java
@@ -1,10 +1,12 @@
 import org.tensorflow.SavedModelBundle;
+import java.io.FileOutputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class ModelLoader {
@@ -14,8 +16,17 @@ public class ModelLoader {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(MODEL_URL)).build();
         byte[] archiveBytes = client.send(request, HttpResponse.BodyHandlers.ofByteArray()).body();
-        Files.write(Paths.get("/tmp/model.zip"), archiveBytes);
-        String exportDir = "/tmp/saved_model";
+        String exportDir = System.getProperty("java.io.tmpdir") + "/saved_model";
+        Files.createDirectories(Paths.get(exportDir));
+        try (ZipInputStream zis = new ZipInputStream(new java.io.ByteArrayInputStream(archiveBytes))) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                String outPath = exportDir + "/" + entry.getName();
+                try (FileOutputStream fos = new FileOutputStream(outPath)) {
+                    zis.transferTo(fos);
+                }
+            }
+        }
         return SavedModelBundle.load(exportDir, "serve");
     }
 }
