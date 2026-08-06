d509894fa2a50fdc68906992803a635b8737029d
Jon Green <jon.green@datadoghq.com>
2026-08-06 11:22:26 -0400

    Fix duplicate finding in vulnerable8: keep only AutoTokenizer sink

--- javascript-supplychain/vulnerable8.js
+++ javascript-supplychain/vulnerable8.js
@@ -1,10 +1,9 @@
-const { AutoModel, AutoTokenizer } = require("@huggingface/transformers");
+const { AutoTokenizer } = require("@huggingface/transformers");
 const express = require("express");
 
 const app = express();
 
 app.post("/load", async (req, res) => {
-  const model = await AutoModel.from_pretrained("org/my-model");
   const tokenizer = await AutoTokenizer.from_pretrained("org/my-model");
   res.json({ status: "loaded" });
 });
--- typescript-supplychain/vulnerable8.ts
+++ typescript-supplychain/vulnerable8.ts
@@ -1,10 +1,9 @@
-import { AutoModel, AutoTokenizer } from "@huggingface/transformers";
+import { AutoTokenizer } from "@huggingface/transformers";
 import express from "express";
 
 const app = express();
 
 app.post("/load", async (req, res) => {
-  const model = await AutoModel.from_pretrained("org/my-model");
   const tokenizer = await AutoTokenizer.from_pretrained("org/my-model");
   res.json({ status: "loaded" });
 });
